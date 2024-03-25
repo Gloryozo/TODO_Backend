18accc4ab753d2881ce325d9d2804a90fcb06ce6
Gloryozo <gloryozoji@gmail.com>
2024-03-25 03:37:57 +0200

    .env created in valut.env and also connected the database using render.com

---
+++ helpers/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const query = (sql,values = []) => 
+{
+    return new Promise((resolve, reject) => 
+    {
+        pool.query(sql, values)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });     
+};
+
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: process.env.SSL
+});
+
+
+module.exports = { query };
